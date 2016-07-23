f79e9a953330508dac7413ddeba2ecd0e83741ab
Ignasi Vegas <ignasivegas@gmail.com>
2016-07-23 00:38:46 -0400

    start bug fixed

--- lapokedex/settings.py
+++ lapokedex/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'lapokedex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'lapokedex',
-        'USER': 'pokedexuser',
-        'PASSWORD': 'pokedex3221',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
