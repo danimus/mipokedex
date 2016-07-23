ea763c37978ea34fc14b59ff4cd2460b92a27c7d
Ignasi Vegas <ignasivegas@gmail.com>
2016-07-23 17:09:16 -0400

    Get GEO and update Map

--- static/js/controllers/MapController.js
+++ static/js/controllers/MapController.js
@@ -1,13 +1,15 @@
 app.controller('MapController', ['$scope','$http','uiGmapLogger','uiGmapGoogleMapApi', function ($scope,$http,$log,GoogleMapApi) {
 
+    //Check if browser has geo activated
     $scope.geolocationAvailable = navigator.geolocation ? true : false;
 
     var style = [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}];
 
+    //Map initial config
     $scope.map = {
         center: {
-            latitude: 34.0098598,
-            longitude: -118.4986249
+            latitude: 40.7829,
+            longitude: 73.9654
         },
         zoom: 15,
         options: {
@@ -23,24 +25,20 @@ app.controller('MapController', ['$scope','$http','uiGmapLogger','uiGmapGoogleMa
         }
     };
 
+    $scope.GetMarkers = function () {
 
-    $scope.findMe = function () {
-        if ($scope.geolocationAvailable) {
+    //Will get markers with actual scope coord values
+        var httpRequest = $http({
+            method: 'GET',
+            dataType:'json',
+            url: '/api/'+$scope.map.center.latitude+'/'+$scope.map.center.longitude
 
-            navigator.geolocation.getCurrentPosition(function (position) {
-
-                $scope.map.center = {
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude
-                };
-
-                $scope.$apply();
-            }, function () {
-            });
-        } else{
-            console.log("Geo not available.");
-        }
+        }).success(function (data) {
+            $scope.pokemonMarkers = (data);
+            
+        });
     };
+
 
     angular.extend($scope, {
         searchbox: {
@@ -58,16 +56,32 @@ app.controller('MapController', ['$scope','$http','uiGmapLogger','uiGmapGoogleMa
 
     GoogleMapApi.then(function(maps) {
 
-        //$scope.findMe();
+        //Get GEO, if not avialable, will use default coord values (Central Park)
+        if ($scope.geolocationAvailable) {
+
+            navigator.geolocation.getCurrentPosition(function (position) {
+
+                //Update scope with new values
+                $scope.map.center = {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                };
+
+                //Submit scope. refresh map, submit new values to map.
+                $scope.$apply();
+
+                //download and print Markers
+                $scope.GetMarkers();
+                
+
+            });
+
+        } else{
+            console.log("Geo not available.");
+            $scope.GetMarkers();
+        }
 
         $scope.pokemonMarkers = [];
-        var httpRequest = $http({
-            method: 'GET',
-            dataType:'json',
-            url: '/api/'+$scope.map.center.latitude+'/'+$scope.map.center.longitude
-
-        }).success(function (data) {
-            $scope.pokemonMarkers = (data);
-        });
+        
     })
 }]);
